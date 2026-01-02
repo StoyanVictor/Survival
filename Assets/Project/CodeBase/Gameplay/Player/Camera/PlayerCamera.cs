b367c67de686fa57a7fc7d8945eb77a74a5d80c2
StoyanVictor <stoianvitea2003@gmail.com>
2026-01-03 01:49:49 +0300

    HUD Layouting

--- Assets/Project/CodeBase/Gameplay/Player/Camera/PlayerCamera.cs
+++ Assets/Project/CodeBase/Gameplay/Player/Camera/PlayerCamera.cs
@@ -6,17 +6,28 @@ namespace Project.CodeBase.Gameplay.Player.Camera {
         
         [SerializeField] private CinemachineFreeLook _cinemachineFreeLook;
         [SerializeField,Range(0,20)] private float _rotationSpeed;
+        
+        [SerializeField] private bool _horizontalInvert;
+        [SerializeField] private bool _verticalInvert;
+        
         private float _horizontalMouseInput;
+        private float _verticalMouseInput;
 
         private void Awake() {
             Cursor.lockState = CursorLockMode.Locked;
         }
-
+        private float GetRotationValue(bool invertCheck,float inputType, float cameraAxis) {
+            if(!invertCheck)
+                return inputType * _rotationSpeed + cameraAxis;
+            return -inputType * _rotationSpeed + cameraAxis;
+        }
         private void Update() {
             _horizontalMouseInput = _cinemachineFreeLook.m_XAxis.m_InputAxisValue;
-            var a = (_horizontalMouseInput * _rotationSpeed) + _cinemachineFreeLook.transform.eulerAngles.y; 
+            _verticalMouseInput = _cinemachineFreeLook.m_YAxis.m_InputAxisValue;
+            var yRotationValue = GetRotationValue(_horizontalInvert,_horizontalMouseInput,_cinemachineFreeLook.transform.eulerAngles.y); 
+            var xRotationValue =GetRotationValue(_verticalInvert,_verticalMouseInput,_cinemachineFreeLook.transform.eulerAngles.x);
             if (_horizontalMouseInput >= 1 || _horizontalMouseInput <= 1) {
-                var _targetRotation = Quaternion.Euler(0,a, 0);
+                var _targetRotation = Quaternion.Euler(xRotationValue,yRotationValue,0);
                 _cinemachineFreeLook.gameObject.transform.rotation = Quaternion.Slerp(_cinemachineFreeLook.gameObject.transform.rotation, 
                     _targetRotation ,_rotationSpeed* Time.deltaTime);
                     // Я пиздатый тип
